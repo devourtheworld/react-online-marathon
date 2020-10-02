0dbe17f680e392360dde5e44f51fdc60cf1fb91d
zebestlove <zbyslav13@gmail.com>
2020-10-02 14:02:15 +0300

    age verification IF + TRY..CATCH

--- sprint-1/task5.js
+++ sprint-1/task5.js
@@ -16,3 +16,28 @@ function checkAdult(age){
     }
     console.log('Age verification complete');
 }
+
+
+function checkAdult(age){
+    try{
+      if (age === undefined){
+        console.log('Error Please, enter your age');
+      } else if (age < 0){
+        console.log('Error Please, enter positive number');
+      } else if (age < 18){
+        console.log('Error Access denied - you are too young!');
+      } else if (typeof(age) === "string"){ 
+          if (age.match(/[a-z]/i)){
+            console.log('Error Please, enter number');
+          }
+      } else if (age%1 > 0){
+        console.log('Error Please, enter Integer number');
+      } else {
+        exception;
+      }
+    } catch (exception){
+        console.log('Access allowed');
+    } finally {
+      console.log('Age verification complete');
+    }
+  }
