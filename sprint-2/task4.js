81446ea6ad27d8f2a7b41e974c54f1e0ca8c7b98
zebestlove <zbyslav13@gmail.com>
2020-10-06 17:28:13 +0300

    task 4 changen variables names

--- sprint-2/task4.js
+++ sprint-2/task4.js
@@ -1,15 +1,15 @@
 const maxInterv = (...numbers) => {
-    let a = 0;
-    let b = [];
-    if (numbers.length <= 1) {
+  let difference = 0;
+  let diffArr = [];
+  if (numbers.length <= 1){
       return 0;
-    }
-    for (let i = 0; i < numbers.length - 1; i++) {
-      a = numbers[i] - numbers[i + 1];
-      if (a < 0) {
-        a = a * -1;
+  }
+  for (let i = 0; i < numbers.length - 1; i++){
+      difference = numbers[i] - numbers[i + 1];
+      if (difference < 0){
+        difference = difference * -1;
       }
-      b.push(a);
+      diffArr.push(difference);
     }
-    return Math.max(...b);
+  return(Math.max(...diffArr));
 };
