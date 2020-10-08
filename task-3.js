0d9bc8848c0402d0ab9535b1036f9501f7b84657
zebestlove <zbyslav13@gmail.com>
2020-10-08 19:48:56 +0300

    movie class (task 3)

---
+++ task-3.js
@@ -0,0 +1,13 @@
+const Checker = require('./Checker.js');
+class Movie {
+   constructor(name, category, startShow){
+       this.name = name;
+       this.category = category;
+       this.startShow = startShow;
+   }
+   watchMovie(){
+       return("I watch the movie " + this.name + "!");
+   }
+}
+const movie1 = new Movie ('Titanic', 'drama', 1997);
+const movie2 = new Movie ("Troya", "historical", 2004);
